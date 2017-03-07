8280b3c21a4db099e712117cf1f90f4eab0f3245
lauraelena33 <lauraelenag33@gmail.com>
2017-03-07 11:08:31 -0500

    Basic UserAccount class

---
+++ Profits R Us/src/UserAccount.java
@@ -0,0 +1,35 @@
+
+public class UserAccount{
+	
+	private String email;
+	private String password;
+
+
+	public UserAccount(){
+		email = null;
+		password = null;
+	} 
+
+	public UserAccount(String email, String password){
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail(){
+		return email;
+	} 
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public void setPassword(String password){
+		this.password = password;
+	}
+
+
+}
