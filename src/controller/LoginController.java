f070bf08acd3b596941e8fd263e255eec2a8b4ee
JonnyLinton <linton.jonny@gmail.com>
2017-03-08 10:34:17 -0500

    adding basic Controller structure

---
+++ src/controller/LoginController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class LoginController implements EventHandler {
+
+    @Override
+    public void handle(Event event) {
+
+    }
+}
