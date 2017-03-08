7256a61e69fe4a6890a88324f0c4d004682a6a91
JonnyLinton <linton.jonny@gmail.com>
2017-03-08 10:16:19 -0500

    added UserAccount

---
+++ src/model/UserAccount.java
@@ -0,0 +1,28 @@
+package model;
+
+public class UserAccount {
+    private String email;
+    private String password;
+
+    public UserAccount() {
+        email = "";
+        password = "";
+    }
+    public UserAccount(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
