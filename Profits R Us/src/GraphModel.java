cb7b6dc39927f28d2394a931f905aa9ce8a81bc3
Palmirouze <Palmirouze@gmail.com>
2017-03-08 16:14:55 -0500

    Formatting for graph model class

--- Profits R Us/src/GraphModel.java
+++ Profits R Us/src/GraphModel.java
@@ -6,14 +6,15 @@ import java.util.Scanner;
 
 import javafx.scene.chart.XYChart;
 
-public class GraphModel {
+public class GraphModel
+{
 	
 	private XYChart.Series<String, Number> stockPriceSeries;
 	
 	//*****THIS NEEDS TO BE UPDATE TO FIT UML MODEL*****
 	
-	public void createStockPriceSeries(int timeline){
-		
+	public void createStockPriceSeries(int timeline)
+	{
 		stockPriceSeries  = new XYChart.Series<String, Number>();
 		String tempDailyStockData;
 		String[] columnDataArray;
@@ -23,7 +24,8 @@ public class GraphModel {
         double lineSmoothing = 0;
         int count=0; 
 		
-        try{
+        try
+		{
         	//Load File csv data into csvFile
 			Scanner csvFile = new Scanner(new File("Sample data.csv"));
 			csvFile.useDelimiter(",");
@@ -32,54 +34,70 @@ public class GraphModel {
 			csvFile.nextLine();
 			
 			//Create LinkedList of Data
-			if(timeline == 0){
-				while(csvFile.hasNextLine()){
+			if(timeline == 0)
+			{
+				while(csvFile.hasNextLine())
+				{
 					tempDailyStockData = csvFile.nextLine();
 					columnDataArray = tempDailyStockData.split(",");
 					allStockValues.add(new Stock(columnDataArray[0], Double.parseDouble(columnDataArray[6])));
 				}
 			}
 			else
-				for(int i = 0; i < timeline*365; i++){
+			{
+				for (int i = 0; i < timeline * 365; i++)
+				{
 					tempDailyStockData = csvFile.nextLine();
 					columnDataArray = tempDailyStockData.split(",");
 					allStockValues.add(new Stock(columnDataArray[0], Double.parseDouble(columnDataArray[6])));
 				}
+			}
 				
 			csvFile.close();
 		}
-    	catch (FileNotFoundException e){
+    	catch (FileNotFoundException e)
+		{
 			e.printStackTrace();
 	    }
-        int dataPointDivider = allStockValues.size()/300;  //Give's us the closest amount of data points to 300 for the graph
-        Stock temp;
-        for (int i = allStockValues.size() ; i > 0 ; i--){
-        	
+
+		//Give's us the closest amount of data points to 300 for the graph
+        int dataPointDivider = allStockValues.size()/300;
+		Stock temp;
+
+        for (int i = allStockValues.size() ; i > 0 ; i--)
+        {
         	temp = allStockValues.removeLast();
         	
-        	if(queueLineSmoothing.size()< dataPointDivider){
+        	if(queueLineSmoothing.size()< dataPointDivider)
+        	{
 				queueLineSmoothing.add(temp.getPrice());
 				lineSmoothing += temp.getPrice();
 			}
-			else{
-				
+			else
+			{
 				if(count%dataPointDivider == 0)
+				{
 					smoothStockValues.add(new Stock(temp.getDate(), lineSmoothing/dataPointDivider));
-				
+				}
+
 				lineSmoothing -= queueLineSmoothing.poll();
 				queueLineSmoothing.add(temp.getPrice());
 				lineSmoothing += temp.getPrice();
 				count++;
-				}
+			}
         }
+
         stockPriceSeries.getData().clear();
-    	for (int i = smoothStockValues.size(); i > 0; i--){
+
+    	for (int i = smoothStockValues.size(); i > 0; i--)
+    	{
     		temp = smoothStockValues.remove();
 			stockPriceSeries.getData().add(new XYChart.Data<String, Number>(temp.getDate(), temp.getPrice()));
     	}
 	}
-	public XYChart.Series<String, Number> getStockPriceSeries(){
-		return stockPriceSeries;
 
+	public XYChart.Series<String, Number> getStockPriceSeries()
+	{
+		return stockPriceSeries;
 	}
 }
