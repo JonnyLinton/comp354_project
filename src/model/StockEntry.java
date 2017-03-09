8a140d8875bf9a6fa7d23f5fe8e374b8aa6622c0
Palmirouze <Palmirouze@gmail.com>
2017-03-09 14:20:49 -0500

    Lots of new functions Added StockEntry class

---
+++ src/model/StockEntry.java
@@ -0,0 +1,41 @@
+package model;
+
+/**
+ * Created by Palmirouze on 2017-03-09.
+ */
+public class StockEntry {
+
+    private String date;
+    private Double value;
+
+    public StockEntry(String date, Double value)
+    {
+        this.date = date;
+        this.value = value;
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public Double getValue()
+    {
+        return value;
+    }
+
+    public void setValue(Double value)
+    {
+        this.value = value;
+    }
+
+    public String toString()
+    {
+        return this.date + " - " + this.getValue();
+    }
+}
