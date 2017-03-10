af2ca94fcecc536ff08b73ad4a28f00c85521c8e
JonnyLinton <linton.jonny@gmail.com>
2017-03-09 21:43:29 -0500

    commenting out Laura's errors

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -23,38 +23,38 @@ public class MainController {
 		currentStock.getPricesInRange(timeLength);
 	}
 	
-	private void graphMovingAverage(ActionEvent event) {
-		//TBD
-		switch(movingAverageButtonPressed.getEventType().getName()) {
-        case "20Day":
-        	if(movingAverageButtonPressed.GETSELECTED() == True)
-        		currentStock.getMovingAverage(MovingAverageInterval.TwentyDay, get);
-        	else
-        		currentStock.getMovingAverage(null, null);
-        	break;
-        case "50day":
-        	if(movingAverageButtonPressed.GETSELECTED() == True)
-        		currentStock.getMovingAverage(MovingAverageInterval.FiftyDay, get);
-        	else
-        		currentStock.getMovingAverage(null, null);
-            break;
-        case "100day":
-        	if(movingAverageButtonPressed.GETSELECTED() == True)
-        		currentStock.getMovingAverage(MovingAverageInterval.HundredDay, get);
-        	else
-        		currentStock.getMovingAverage(null, null);
-            break;
-        case "200day":
-        	if(movingAverageButtonPressed.GETSELECTED() == True)
-        		currentStock.getMovingAverage(MovingAverageInterval.TwoHundredDay, get);
-        	else
-        		currentStock.getMovingAverage(null, null);
-            break; 
-    }
+//	private void graphMovingAverage(ActionEvent event) {
+//		//TBD
+//		switch(movingAverageButtonPressed.getEventType().getName()) {
+//        case "20Day":
+//        	if(movingAverageButtonPressed.GETSELECTED() == True)
+//        		currentStock.getMovingAverage(MovingAverageInterval.TwentyDay, get);
+//        	else
+//        		currentStock.getMovingAverage(null, null);
+//        	break;
+//        case "50day":
+//        	if(movingAverageButtonPressed.GETSELECTED() == True)
+//        		currentStock.getMovingAverage(MovingAverageInterval.FiftyDay, get);
+//        	else
+//        		currentStock.getMovingAverage(null, null);
+//            break;
+//        case "100day":
+//        	if(movingAverageButtonPressed.GETSELECTED() == True)
+//        		currentStock.getMovingAverage(MovingAverageInterval.HundredDay, get);
+//        	else
+//        		currentStock.getMovingAverage(null, null);
+//            break;
+//        case "200day":
+//        	if(movingAverageButtonPressed.GETSELECTED() == True)
+//        		currentStock.getMovingAverage(MovingAverageInterval.TwoHundredDay, get);
+//        	else
+//        		currentStock.getMovingAverage(null, null);
+//            break;
+//    }
 		
 		
 		
-	}
+//	}
 	private void getRecommendations(String recom){
 		
 	}
